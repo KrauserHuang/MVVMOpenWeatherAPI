df33ccf5c2fcaead5260a7468e59ad674fdf1845
krauserhuang <crescentdence@gmail.com>
2022-06-01 14:02:50 +0800

    Initial Commit

---
+++ MVVMOpenWeatherAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMOpenWeatherAPI
+//
+//  Created by Tai Chin Huang on 2022/6/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
